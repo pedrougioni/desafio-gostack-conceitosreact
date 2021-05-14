f6fc83c772ac159c937b3a1cdc4e6f1bb2d02a58
Pedro Ugioni <pedrohrugioni@hotmail.com>
2021-05-14 17:58:30 -0300

    Possibilitar listagem, criação e remoção dos repositorios utilizando a api criada no desafio anterior da rocketseat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+
+  useEffect(()=>{api.get("repositories").then(response => setRepositories(response.data))},[]);
+
+  const [repositories, setRepositories] = useState([]);
+  
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories",{title: `Teste ${Date.now()}`, url: "aaa", techs:["dusahda"]});
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    if(response.status === 204){
+      const repIndex = repositories.findIndex(rep => rep.id === id);
+      const newRepositories = repositories.slice();
+      newRepositories.splice(repIndex,1)
+      setRepositories(newRepositories);
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(repository =>
+          <li key={repository.id}>
+             {repository.title}
+             <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
